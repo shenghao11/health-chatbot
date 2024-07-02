3aa6302c54588a1ca5ebeff78ebc68fe2b961d69
shenghao11 <70162831+shenghao11@users.noreply.github.com>
2024-07-01 20:25:16 -0400

    Update web.py

--- web.py
+++ web.py
@@ -75,5 +75,4 @@ if st.session_state.messages[-1]["role"] != "assistant":
             placeholder.markdown(full_response)
     message = {"role": "assistant", "content": full_response}
     st.session_state.messages.append(message)
-    message = {"role": "assistant", "content": full_response}
-    st.session_state.messages.append(message)
+    
