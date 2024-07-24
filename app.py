035c2709993651ce3a714fc5ac80477bb9fea0f9
shenghao11 <70162831+shenghao11@users.noreply.github.com>
2024-07-24 14:05:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
+            header {visibility: hidden;}
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
